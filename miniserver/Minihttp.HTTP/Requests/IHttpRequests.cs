325ffd1d4eec003baf305ae5af35d4e748e1def9
AndyPeep <78352932+AndyPeep@users.noreply.github.com>
2021-10-29 09:55:29 +0300

    Add files via upload

---
+++ miniserver/Minihttp.HTTP/Requests/IHttpRequests.cs
@@ -0,0 +1,18 @@
+﻿using Minihttp.HTTP.Enums;
+using Minihttp.HTTP.Headers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minihttp.HTTP.Requests
+{
+    interface IHttpRequest
+    {
+        string Path { get; }
+        string Url { get; }
+        Dictionary<string, object> FormData { get; }
+        Dictionary<string, object> QueryData { get; }
+        IHttpHeaderCollection Header { get; }
+        HttPReguestMethod RequestMethod { get; }
+    }
+}
---
+++ miniserver/Minihttp.HTTP/Requests/IHttppRequest.cs
@@ -0,0 +1,54 @@
+﻿using Minihttp.HTTP.Common;
+using Minihttp.HTTP.Enums;
+using Minihttp.HTTP.Headers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minihttp.HTTP.Requests
+{
+    class HttpRequest : IHttpRequest
+    {
+        public HttpRequest(string requestString)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(requestString, nameof(requestString));
+            this.FormData = new Dictionary<string, object>();
+            this.QueryData = new Dictionary<string, object>();
+            this.Headers = new HttpHeaderCollection();
+
+        }
+        public string Path { get; }
+
+        public string Url { get; }
+
+        public Dictionary<string, object> FormData { get; }
+
+        public Dictionary<string, object> QueryData { get; }
+
+        public IHttpHeaderCollection Headers { get; }
+
+        public HttPReguestMethod RequestMethod { get; }
+
+        private bool IsValidRequestLine(string[] requestLine) { }
+
+        private bool IsValidReguestLQueryString(string queryString, string[] queryParameters) { }
+
+        private void ParseRequestMethod(string[] requestLine) { }
+
+        private void ParseRequestUrl(string[] requestLine) { }
+
+        private void ParseRequestPath() { }
+
+        private void ParseHeaders(string[] requestContent) { }
+
+        private void ParseCookies() { }
+
+        private void ParseQueryParameters() { }
+
+        private void ParseFormDataParameters(string fromData) { }
+
+        private void ParseRequstParameters(string fromdata) { }
+
+        private void ParseRequest(string requestString) { }
+    }
+}
