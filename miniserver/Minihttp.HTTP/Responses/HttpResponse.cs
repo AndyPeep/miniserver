64dcd485b08301f5d4bb50b1fa7f0a85891afc31
AndyPeep <78352932+AndyPeep@users.noreply.github.com>
2021-10-29 09:54:26 +0300

    Update HttpResponse.cs

--- miniserver/Minihttp.HTTP/Responses/HttpResponse.cs
+++ miniserver/Minihttp.HTTP/Responses/HttpResponse.cs
@@ -1,4 +1,5 @@
-﻿using Minihttp.HTTP.Enums;
+using Minihttp.HTTP.Common;
+using Minihttp.HTTP.Enums;
 using Minihttp.HTTP.Headers;
 using System;
 using System.Collections.Generic;
@@ -8,30 +9,51 @@ namespace Minihttp.HTTP.Responses
 {
     class HttpResponse : IHttpResponse
     {
-        public HttpHeader() 
+        public HttpResponse()
         {
-
+            this.Headers = new HttpHeaderCollection();
+            this.Content = new byte[0];
         }
 
         public HttpResponse(HttpResponseStatusCode statusCode)
+            : this()
         {
-
+            CoreValidator.ThrowIfNull(statusCode, nameof(statusCode));
+            this.StatusCode = statusCode;
         }
 
-        public HttpResponseStatusCode Response StatusCode { get; private set;}
+        public HttpResponseStatusCode StatusCode { get; private set; }
 
-        public IHttpHeaderCollection Response Headers { get; private set;}
+        public IHttpHeaderCollection Headers { get; private set; }
 
-        public byte[] Content { get; private set; };
+        public byte[] Content { get; private set; }
 
         public void AddHeder(HttpHeader header)
         {
-            throw new NotImplementedException();
+            CoreValidator.ThrowIfNull(header, nameof(header));
+            this.Headers.Add(header);
         }
 
-        public byte[] IGetBytes()
+        public byte[] GetBytes()
         {
-            throw new NotImplementedException();
+            byte[] header = Encoding.UTF8.GetBytes(this.ToString());
+            byte[] response = new byte[header.Length + Content.Length];
+            Array.Copy(header, 0, response, 0, header.Length);
+            Array.Copy(Content, 0, response, header.Length, Content.Length);
+            return response;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result
+                .Append($"{GlobalConstants.HttpOneProtocolFragment} {(int)this.StatusCode} {this.StatusCode}")
+                .Append($"{GlobalConstants.HttpNewLine}")
+                .Append($"{this.Headers}")
+                .Append($"{GlobalConstants.HttpNewLine}");
+            result.Append($"{GlobalConstants.HttpNewLine}");
+
+            return base.ToString();
         }
     }
 }
